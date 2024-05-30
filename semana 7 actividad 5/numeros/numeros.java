113ffdec2e6acfae2dd033e0570716eb3c94dbb4
JoncesdeZL <163427101+JONZLCESDE@users.noreply.github.com>
2024-05-29 19:41:02 -0500

    Add files via upload

---
+++ semana 7 actividad 5/numeros/numeros.java
@@ -0,0 +1,83 @@
+package com.numeros;
+
+public class numeros {
+    
+    public String ejercicio1(int num1, int num2) {
+        
+        if (num1>num2) {
+          return "el numero mayor es: num1" ;
+        } 
+        else if (num2>num1) {
+            return "el numero mayor es: num2" ;
+        }
+        return "los dos numeros son iguales";
+    }
+    
+    public String ejercicio2(double calificacion) {
+      
+       if (calificacion>=3.5 && calificacion<=5.0) {
+        return "bien hecho, el examen ha sido aprobado";
+       }
+        else if (calificacion>=6&& calificacion<=100) {
+            return "calificacion incorrecta";
+        }
+        return "nop,usted perdio el examen";
+    }
+
+    
+    /**
+     * @param precio
+     * @return
+     */
+    public double ejercicio3(double precio) {
+        
+        if (precio>=50.000)  {
+           double descuento = 0.1;
+        return  precio -(precio* descuento );
+        }
+        else{
+            return 0;
+        }
+       
+    }
+    
+    /**
+     * @param tipoFigura
+     * @param ladoCuadrado
+     * @param baseTriangulo
+     * @param alturaTriangulo
+     * @param radioCirculo
+     * @return
+     */
+    public double ejercicio4(int tipoFigura, double ladoCuadrado, double baseTriangulo, double alturaTriangulo,
+            double radioCirculo) {
+        
+        double area = 0;
+       switch (tipoFigura) {
+        case 1:
+            area = ladoCuadrado * ladoCuadrado;
+            break;
+       case 2:
+            area =  baseTriangulo * alturaTriangulo / 2;
+            break;
+       case 3:
+           area = Math.PI * radioCirculo * radioCirculo;
+           break;
+       }
+       return area;
+       
+       }
+    
+            
+    public int ejercicio5(int num1, int num2, int num3) {
+        
+        if (num1<num2&&num1<num3) {
+        return num1;
+        }
+        else if (num2<num1&&num2<num3) {
+            return num2;
+        }
+
+        return num3;
+    }
+}
